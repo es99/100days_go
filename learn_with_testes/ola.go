fe4f5899f3d10aad267dcafd563b756c14167ad2
Engels Souza <engels.franca@icloud.com>
2024-05-01 00:52:35 -0300

    day 32 - inserindo testes por conta propria

--- learn_with_testes/ola.go
+++ learn_with_testes/ola.go
@@ -9,6 +9,11 @@ func Ola(nome string) string {
 	return "Hello, " + nome
 }
 
+func Media(x, y, z float64) float64 {
+	return (x + y + z) / 3
+}
+
 func main() {
 	fmt.Println(Ola("Engels"))
+	fmt.Println(Media(10, 75, 80))
 }
--- learn_with_testes/ola_test.go
+++ learn_with_testes/ola_test.go
@@ -12,3 +12,12 @@ func TestOla(t *testing.T) {
 		t.Errorf("resultado: %s, esperado: %s", resultado, esperado)
 	}
 }
+
+func TestMedia(t *testing.T) {
+	resultado := Media(100, 100, 100)
+	esperado := 100
+
+	if float64(resultado) != float64(esperado) {
+		t.Errorf("resultado %v, esperado %v", resultado, esperado)
+	}
+}
