3ea5392e7b30f0e4588b8dca91ac29556cc091e5
Engels Souza <engels.franca@icloud.com>
2024-05-03 23:40:26 -0300

    day 33 - aprendizado sobre testes na branch main

--- learn_with_testes/ola.go
+++ learn_with_testes/ola.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 )
 
+const espanhol = "espanhol"
+const prefixHelloEspanhol = "Hola, "
+const prefixHelloPortuguese = "Hello, "
+
 // Ola retorna uma string
-func Ola(nome string) string {
-	return "Hello, " + nome
+func Ola(nome, idioma string) string {
+	if nome == "" {
+		nome = "world!"
+	}
+	if idioma == espanhol {
+		return prefixHelloEspanhol + nome
+	}
+	return prefixHelloPortuguese + nome
 }
 
 func main() {
-	fmt.Println(Ola("Engels"))
+	fmt.Println(Ola("Engels", ""))
 }
--- learn_with_testes/ola_test.go
+++ learn_with_testes/ola_test.go
@@ -5,10 +5,32 @@ import (
 )
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Chris")
-	esperado := "Hello, Chris"
 
-	if resultado != esperado {
-		t.Errorf("resultado: %s, esperado: %s", resultado, esperado)
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado: %s\nesperado: %s\n", resultado, esperado)
+		}
 	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Chris", "")
+		esperado := "Hello, Chris"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("", "")
+		esperado := "Hello, world!"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em espanhol", func(t *testing.T) {
+		resultado := Ola("Engels", "espanhol")
+		esperado := "Hola, Engels"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
 }
